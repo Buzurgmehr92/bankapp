e997b95a1ef23f3286a3078a4853bfbce1e28e71
Buzurg <buzurgmehr.r@gmail.com>
2020-09-21 10:03:38 +0500

    Добавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,35 @@
+package types
+
+//Money представляет собой денежную сумму в минимальных (центы, копейки, дирамы и т. д.)
+type Money int64
+
+//Currency представляет код валюты
+type Currency string
+
+//Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+//PAN представляет информацию о платёжной карте
+type PAN string
+
+//Сфкв представляет информацию о платёжной карте
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	MinBalance Money
+	Active     bool
+}
+
+//Payment представляет информацию о платеже
+type Payment struct {
+	ID     int
+	Amount Money
+}
